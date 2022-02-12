e4a97a3c0caaf9bafd6a3cb95f18ecefc2efa0b7
gautam351 <gautampraveen351@gmail.com>
2022-02-12 13:27:09 +0530

    make it better

--- backEnd/server.js
+++ backEnd/server.js
@@ -10,12 +10,12 @@ connectDatabase();
 
 // deployement
 __dirname=path.resolve();
-if(NODE_ENV==+"production"){
+
     app.use(express.static(path.join(__dirname,"/frontend/build")));
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
     })
-}
+
 
 
 app.listen(port,()=>{
