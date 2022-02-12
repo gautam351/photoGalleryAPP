593804f25dbd467c4d33fd64f852ed181ffa30ba
gautam351 <gautampraveen351@gmail.com>
2022-02-12 14:36:53 +0530

    last

--- backEnd/server.js
+++ backEnd/server.js
@@ -11,7 +11,7 @@ connectDatabase();
 // deployement
 
 
-  if(process.env.NODE_ENV==="production"){  app.use(express.static(path.join(__dirname,"/frontend/build")));
+  if(process.env.NODE_ENV==="production"){  app.use(express.static(path.join(__dirname,"../frontend/build")));
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,"../frontend","build","index.html"))
        
